0c42e8e93f4fae6f0b3b969f81672b600f416c12
yareckon <tek@castyour.net>
2017-05-30 03:10:34 +0200

    Check for body content type with workaround.

--- src/CssInlinerPlugin.php
+++ src/CssInlinerPlugin.php
@@ -41,6 +41,20 @@ class CssInlinerPlugin implements Swift_Events_SendListener
 		if ($message->getContentType() === 'text/html') {
 			$message->setBody($this->converter->convert($message->getBody()));
 		}
+		
+		if ( $message->getContentType() === 'multipart/alternative'  )  {
+			// we make a copy of the message, and  remove all parts and then check the contenttype to get the content type of the body
+			$fake_message = clone $message;
+			$fake_parts = $fake_message->getChildren();
+			foreach ($fake_parts as $part)
+				{
+				   $fake_message->detach($part);
+				}
+			$deduced_content_type = $fake_message->getContentType();
+		  if ($deduced_content_type === 'text/html' ) {  // do what we did above!
+				$message->setBody($this->converter->convert($message->getBody()));
+			}
+	 	 }
 
 		foreach ($message->getChildren() as $part) {
 			if (strpos($part->getContentType(), 'text/html') === 0) {
